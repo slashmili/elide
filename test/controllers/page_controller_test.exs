6e1461f661ddbfea28efc6fe6f44bc78156730e1
Milad Rastian <eslashmili@gmail.com>
2016-01-15 09:50:33 +0800

    Upgraded to phoenix 1.1.2

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule Elide.PageControllerTest do
   use Elide.ConnCase
 
-  test "GET /" do
-    conn = get conn(), "/"
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -21,8 +21,9 @@ defmodule Elide.ConnCase do
       use Phoenix.ConnTest
 
       alias Elide.Repo
-      import Ecto.Model
-      import Ecto.Query, only: [from: 2]
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query, only: [from: 1, from: 2]
 
       import Elide.Router.Helpers
 
@@ -36,6 +37,6 @@ defmodule Elide.ConnCase do
       Ecto.Adapters.SQL.restart_test_transaction(Elide.Repo, [])
     end
 
-    :ok
+    {:ok, conn: Phoenix.ConnTest.conn()}
   end
 end
