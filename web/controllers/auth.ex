75996ab2bbbf71893f7969a337d1c47866f01a95
Milad Rastian <eslashmili@gmail.com>
2016-01-19 23:04:38 +0800

    Added test for auth plug

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -28,4 +28,17 @@ defmodule Elide.Auth do
       |> halt()
     end
   end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def logout(conn) do
+    conn
+    |> put_session(:user_id, nil)
+    |> configure_session(renew: true)
+  end
 end
--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -47,9 +47,7 @@ defmodule Elide.AuthController do
     user = get_user!(provider, token)
 
     conn
-    |> assign(:current_user, user)
-    |> put_session(:user_id, user.id)
-    |> configure_session(renew: true)
+    |> Elide.Auth.login(user)
     |> redirect(to: "/")
   end
 
