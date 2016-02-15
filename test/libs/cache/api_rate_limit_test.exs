f15361a468406947f7651e0d2eee17ffde0d37e0
Milad Rastian <eslashmili@gmail.com>
2016-02-15 19:48:24 +0800

    Adjusted test to match timing with ttl and ttl_check

--- test/libs/cache/api_rate_limit_test.exs
+++ test/libs/cache/api_rate_limit_test.exs
@@ -20,18 +20,18 @@ defmodule Elide.Cache.ApiRateLimitTest do
 
   test "should allow access after the time period passed" do
     {:ok, pid} = ApiRateLimit.start_link([
-      ttl_check: 500,
-      ttl: 500,
+      ttl_check: 200,
+      ttl: 200,
       rate_limit: 2
     ])
 
     assert ApiRateLimit.allowed?("127.0.0.1", pid), "First access should be allowed"
     #TODO: find a better way to simulate the time movement
-    :timer.sleep(300)
+    :timer.sleep(100)
     assert ApiRateLimit.allowed?("127.0.0.1", pid), "Second access should be allowed"
     refute ApiRateLimit.allowed?("127.0.0.1", pid), "Third access in same time period should be denied"
-    :timer.sleep(1500)
-    assert ApiRateLimit.allowed?("127.0.0.1", pid), "Forth access happens in new time window"
+    :timer.sleep(400)
+    assert ApiRateLimit.allowed?("127.0.0.1", pid), "Forth access happens in new time window shoud be allowed"
 
     pid |> ApiRateLimit.stop
   end
