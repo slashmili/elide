f5af32735ecd0b3e7286239b04b115ab0cf31744
Milad Rastian <eslashmili@gmail.com>
2016-02-27 13:01:34 +0800

    Used Timex in StatServer
    Timex has helper functions that helps to deal with date and time

--- lib/elide/stat_server.ex
+++ lib/elide/stat_server.ex
@@ -1,17 +1,40 @@
 defmodule Elide.StatServer do
+  use Timex
+
   def inc(opts) do
     data = %{
       country: opts[:country], platform: opts[:platform],
       elink_id: opts[:elink].id, browser: opts[:browser],
       referrer: opts[:referrer], visiting_interval: opts[:visited_at]
     }
+    visiting_interval =
+      opts[:visited_at]
+      |> reset_hour
+      |> to_tuple
+
+    created_at =
+      Date.now
+      |> to_tuple
+
     data = %{
       elink_id: opts[:elink].id, tag: "browser",
-      value: opts[:browser], visiting_interval: opts[:visited_at],
-      inserted_at: {{2012, 10, 10},{15,40,59, 0}}, updated_at: {{2012, 10, 10},{15,40,59, 0}},
+      value: opts[:browser], visiting_interval: visiting_interval,
+      inserted_at: created_at, updated_at: created_at,
       count: 1
     }
     query = Elide.QueryBuilder.upsert_stats(Map.keys(data))
     {:ok, _} = Ecto.Adapters.SQL.query(Elide.Repo, query, Map.values(data))
+  end
+
+  defp to_tuple(%DateTime{} = date) do
+    %DateTime{year: y, month: m, day: d, hour: h, minute: min, second: s, ms: ms} = date
+    {{y, m, d}, {h, min, s, round(ms * 1_000)}}
+  end
+
+  defp reset_hour(%DateTime{} = date) do
+    date
+    |> Map.put(:minute, 0)
+    |> Map.put(:second, 0)
+    |> Map.put(:ms, 0)
   end
 end
--- test/libs/stat_server_test.exs
+++ test/libs/stat_server_test.exs
@@ -12,13 +12,26 @@ defmodule Elide.StatServerTest do
     {:ok, elink: elink, domain: domain}
   end
 
+  test "save visiting interval by trimming min, sec and ms", %{elink: elink} do
+    date = Timex.Date.from({{2012, 10, 10}, {15, 40, 59, 0}})
+    StatServer.inc(
+    elink: elink, browser: "Chrome",
+    country: "MY", referrer: "http://example.com",
+    platform: "Mac", visited_at: date
+    )
+
+    stat = Repo.get_by(Stat, %{elink_id: elink.id, tag: "browser"})
+    assert stat.visiting_interval == Timex.Date.from({{2012, 10, 10}, {15, 0, 0, 0}})
+  end
+
   test "increase stat count in the same hour", %{elink: elink} do
     (1..10)
     |> Enum.each(fn(_) ->
+      date = Timex.Date.from({{2013, 11, 1}, {11, 30, 59, 7}})
       StatServer.inc(
         elink: elink, browser: "Chrome",
         country: "MY", referrer: "http://example.com",
-        platform: "Mac", visited_at: {{2012, 10, 10},{15,40,59, 0}}
+        platform: "Mac", visited_at: date
       )
     end)
     stat = Repo.get_by(Stat, %{elink_id: elink.id, tag: "browser"})
